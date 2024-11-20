e4a1d206badd4d9b6275bbbe06ec1c535c6d696f
Andrew Abramov <andrabra@yandex.ru>
2024-11-20 16:21:29 +0500

    DEV-1: Сделал заготовку для верстки внутренностей формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,9 +3,30 @@ import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useEffect, useRef, useState } from 'react';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
-	const [isOpen, setIsOpen] = useState(false);
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (params: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(true);
+
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [width, setWidth] = useState(articleState.contentWidth);
+	const [bgColor, setBgColor] = useState(articleState.backgroundColor);
+
 	const aside = useRef<HTMLDivElement>(null);
 	const handlerClickBtn = () => {
 		return setIsOpen(!isOpen);
@@ -26,6 +47,17 @@ export const ArticleParamsForm = () => {
 		};
 	}, [isOpen]);
 
+	const handleButtonSubmit = (): void => {
+		setArticleState({
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			backgroundColor: bgColor,
+			contentWidth: width,
+			fontSizeOption: fontSize,
+		});
+		handlerClickBtn();
+	};
+
 	return (
 		<>
 			<ArrowButton
@@ -42,6 +74,15 @@ export const ArticleParamsForm = () => {
 						: styles.container
 				}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={(option) => setFontFamily(option)}
+						title='Шрифт'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
