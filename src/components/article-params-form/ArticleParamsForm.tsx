ad48fe8b39d08e56fe7f8dc57b3a0f629220810e
Andrew Abramov <andrabra@yandex.ru>
2024-11-20 15:43:08 +0500

    DEV-1: Реализовал логику открытия и закрытия модалки

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,45 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const aside = useRef<HTMLDivElement>(null);
+	const handlerClickBtn = () => {
+		return setIsOpen(!isOpen);
+	};
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (aside.current && !aside.current.contains(event.target as Node)) {
+			handlerClickBtn();
+		}
+	};
+
+	useEffect(() => {
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					handlerClickBtn();
+				}}
+			/>
+			<aside
+				ref={aside}
+				className={
+					isOpen
+						? styles.container_open + ' ' + styles.container
+						: styles.container
+				}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
